25ac31b52084aa6936654974c84242f680365c11
mythilibala <47290698+mythilibala@users.noreply.github.com>
2019-06-04 14:06:13 +0530

    Update sumoffirstn.py

--- code/sumoffirstn.py
+++ code/sumoffirstn.py
@@ -1,10 +1,10 @@
-n=int(input())
-k=int(input())
-m=[]
-for i in range(n):
-  j=int(input()) 
-  m.append(j)
-s=0  
+n,k=input().split(" ")
+n=int(n)
+k=int(k)
+
+m=map(int,input().split(" "))
+m=list(m)
+s=0 
 for i in range(k):
     s=s+m[i]
 print(s)    
