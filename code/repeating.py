7047815602db65833baa2269ab4cfcec30d3dbee
mythilibala <47290698+mythilibala@users.noreply.github.com>
2019-06-10 22:01:26 +0530

    Create repeating.py

---
+++ code/repeating.py
@@ -0,0 +1,13 @@
+n=int(input())
+m=list(map(int,input().split(" ")))
+d={}
+for key in m:
+    d[key]=m.count(key)
+
+l=[]
+for key,values in d.items():
+    if(d[key]>1):
+        l.append(key)
+l.sort()
+for i in l:
+    print(i,end=" ")    
